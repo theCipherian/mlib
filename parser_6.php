2e5697a0e85eee95d30efeaabb0eded2d4336106
theCipherian <thecipheriansvault@gmail.com>
2023-03-16 12:29:04 +0100

    changes

--- parser_6.php
+++ parser_6.php
@@ -2,8 +2,10 @@
 session_start();
 include("init_2.php");
 
+$uni = mt_rand(000000,999999);
+
 if(isset($_POST['current_page'], $_POST['book'])){
     $current_page = $_POST['current_page'];
     $book = $_POST['book'];
-    $query = mysqli_query($init, "UPDATE last_read SET unique_id = '$uni', book = '$book', page = '$current_page', user = '$key_2'");
+    $query = mysqli_query($init, "UPDATE last_read SET unique_id = '$uni', book = '$book', page = '$current_page', user = '$key'");
 }
